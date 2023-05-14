9e502c85422a202d33919a0e2b954f6bee96f99e
robinhwp <89498570+robinhwp@users.noreply.github.com>
2023-05-15 07:46:04 +0900

    aa

--- MultivariateDataAnalysis/4.R
+++ MultivariateDataAnalysis/4.R
@@ -7,3 +7,17 @@ str(beer.data)
 zbeer=scale(beer.data)
 round(apply(zbeer, 2, mean), 3)
 round(apply(zbeer, 2, sd), 3)
+
+
+
+auto = read.csv("./data/auto.csv")
+head(auto)
+
+X=auto[,-1]
+# z-standardization
+zX = scale(X, center = TRUE, scale = TRUE)
+# 0-1 transformation
+maxX = apply(X, 2, max)
+minX = apply(X, 2, min)
+z01X = scale(X, center = minX, scale = maxX - minX)
+
--- MultivariateDataAnalysis/data/nreadMatrix.r
+++ MultivariateDataAnalysis/data/nreadMatrix.r
@@ -1,15 +1,35 @@
+readMatrix = function(datam, nrows, cname, lower=1)
+{
+  # data.m = scan(datam)
+  # lower=1 : Lower Triangluar Matrix (default)
+  # lower=2 : Upper Triangluar Matrix
+  n = nrows
+  
+  if(lower ==1)
+  {
+    DistanceArray = array(0, n*(n-1)/2)
+    for(i in 1:(n))
+      for(j in (i+1):n)
+      {
+        kk1 = (j-1)*(j-2)/2 + i
+        kk2 = n*(i-1) - i*(i-1)/2 + j-i
+        DistanceArray[kk2] = datam[kk1]
+      }
+  }
+}
 readMatrix <- function(datam, nrows, cname, lower=1)
-{ 
-        # data.m <- scan(datam)
-        # lower=1 : Lower  Triangular Matrix (default)
-        # lower=2 : upper  Triangular Matrix 
-        n <- nrows
+{
+  # data.m <- scan(datam)
+  # lower=1 : Lower  Triangular Matrix (default)
+  # lower=2 : upper  Triangular Matrix 
+  n <- nrows
 
-        if( lower == 1)
-        {  DistanceArray <- array(0, n*(n-1)/2 )
-           for(i in 1:(n-1) )
-             for(j in (i+1):n )
-                 { kk1 <- (j-1)*(j-2)/2 + i
+  if(lower == 1)
+    {
+    DistanceArray <- array(0, n*(n-1)/2 )
+          for(i in 1:(n-1) )
+            for(j in (i+1):n )
+              { kk1 <- (j-1)*(j-2)/2 + i
                    kk2 <- n*(i-1)- i*(i-1)/2 + j-i
                    DistanceArray[kk2] <- datam[kk1] }
         }
